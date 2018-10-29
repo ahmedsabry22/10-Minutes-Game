21a9b5d7c2082b39487febe233de4a49cc2a98bf
Ahmed Sabry <ahmedsabry19@outlook.com>
2018-10-29 15:00:12 +0200

    Initial Commit

---
+++ Assets/EnemySpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+    public GameObject[] enemyPrefabs;
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("SpawnEnemy", 2, 2);
+	}
+
+    private void SpawnEnemy()
+    {
+        int prefabIndex = Random.Range(0, enemyPrefabs.Length);
+
+        Vector3 pos = new Vector3(Random.Range(-5f, 5f), 6, 0);
+
+        GameObject enemy = Instantiate(enemyPrefabs[prefabIndex], pos, Quaternion.identity);
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        transform.position = new Vector3(pos.x, transform.position.y, 0);
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Destroy(gameObject);
+            Debug.Log("You lose");
+        }
+    }
+}
